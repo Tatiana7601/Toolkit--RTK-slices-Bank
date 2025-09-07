53b1921d5b617b2f2f6f6a105eeec6d7b520a633
Tetiana Palazhevych <paltatyana1@gmail.com>
2025-09-07 20:01:08 +0200

    Delete src/configureStore directory

--- src/configureStore/store.js
+++
@@ -1,8 +0,0 @@
-import {createStore} from "../redux/createStore.js";
-import {accountReducer} from "../reducer/accountReducer.js";
-
-const initialStore = {
-    balance: 0
-};
-
-export const store = createStore(accountReducer, initialStore);
